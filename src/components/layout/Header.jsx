0b66694a43e07cc99e90a0910d53e5c739c03308
Kariukikyle <kylekariuki93@gmail.com>
2025-11-15 16:30:28 +0300

    Update Header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <h1 className="text-3xl font-extrabold tracking-wider">
-      KYLEMOTORS<span className="text-red-500">.KE</span>
+      KYLECARS.KE<span className="text-red-500">.KE</span>
     </h1>
   )
 }
